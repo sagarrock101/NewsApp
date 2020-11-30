6d828ddcb03a074263123fd07dfd2830ee675c83
sagarrock101 <nsagarrock101@gmail.com>
2020-11-30 21:52:19 +0530

    navigating from main app module to nav headlines module

--- app/src/main/java/com/sagarock101/newsapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sagarock101/newsapp/ui/activities/MainActivity.kt
@@ -2,12 +2,18 @@ package com.sagarock101.newsapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavGraph
+import androidx.navigation.NavHost
 import com.sagarock101.newsapp.R
+import com.sagarock101.newsapp.interfaces.NavigationListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun navigate() {
     }
 }
--- app/src/main/java/com/sagarock101/newsapp/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/sagarock101/newsapp/ui/fragments/SplashFragment.kt
@@ -3,13 +3,16 @@ package com.sagarock101.newsapp.ui.fragments
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavGraph
 import androidx.navigation.fragment.findNavController
 import com.sagarock101.newsapp.R
 import com.sagarock101.newsapp.databinding.FragmentSplashBinding
+import java.lang.Exception
 
 class SplashFragment : Fragment() {
 
@@ -22,9 +25,21 @@ class SplashFragment : Fragment() {
     ): View? {
         binding = FragmentSplashBinding.inflate(inflater)
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
-            findNavController().navigate(R.id.action_splashFragment_to_navigation_news_headlines)
+            navigateToNewsHeadlines()
         }, 1000)
         return binding.root
+    }
+
+    private fun navigateToNewsHeadlines() {
+        try {
+            val graph = findNavController().graph.findNode(R.id.navigation_news_headlines)
+            if (graph is NavGraph) {
+                graph.startDestination = R.id.newsHeadlinesFragment
+            }
+            findNavController().navigate(R.id.action_splashFragment_to_newsHeadlinesFragment3)
+        } catch (e: Exception) {
+            Log.e(this.javaClass.name, "${e.message}")
+        }
     }
 
     override fun onPause() {
