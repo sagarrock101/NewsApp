173cfe411f6f7fb49e26e3a082ef70c2d6e51087
sagarrock101 <nsagarrock101@gmail.com>
2021-02-20 16:05:45 +0530

    Yes now widget_item.xml its appearing as proper stack view

--- widget/src/main/java/com/sagarock101/widget/StackRemoteViewsFactory.kt
+++ widget/src/main/java/com/sagarock101/widget/StackRemoteViewsFactory.kt
@@ -1,10 +1,14 @@
 package com.sagarock101.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.sagarock101.database.NewsDatabaseRepo
 import com.sagarock101.database.model.Articles
 import timber.log.Timber
@@ -31,11 +35,15 @@ class StackRemoteViewsFactory(
 
     override fun hasStableIds() = true
 
+    @SuppressLint("CheckResult")
     override fun getViewAt(position: Int) = RemoteViews(context.packageName, R.layout.widget_item).apply {
         setTextViewText(R.id.tv_news_title, listOfArticles?.get(position)?.title)
         try {
+            val requestOptions = RequestOptions()
+            requestOptions.transform(CenterCrop(), RoundedCorners(10))
             val bitmap = Glide.with(context).asBitmap()
                 .load(listOfArticles?.get(position)?.urlToImage)
+                .apply(requestOptions)
                 .submit().get()
             setImageViewBitmap(R.id.iv_image, bitmap)
         } catch (e: Exception) {
